ceb36f1f496347775bfcc678d223f326ab019488
valentingrs <valentin.gros06@gmail.com>
2024-12-18 22:56:48 +0100

    modifs

--- Projet/Dictionnaire.cs
+++ Projet/Dictionnaire.cs
@@ -2,17 +2,19 @@ namespace Projet
 {
     public class Dictionnaire
     {
-
         #region Attributs
+
         //private readonly string[] mots;
         private readonly List<string> mots;
         private Dictionary<int, List<string>> motsParLongueur;
         private Dictionary<char, List<string>> motsParLettres;
         private string langue;
+
         #endregion Attributs
 
         public Dictionnaire(string langue)
         {
+
             this.langue = langue;
             this.mots = LireFichierMots($"MotsPossibles{langue.ToUpper()}.txt");
             this.motsParLongueur = TriMotsParLongueur();
@@ -20,32 +22,42 @@ namespace Projet
         }
 
         #region Proprietes
+
         public string Langue
         {
+
             get { return langue; }
             set { langue = value; }
         }
 
         public List<string> Mots
         {
+
             get { return mots; }
         }
+
         #endregion Proprietes
 
-
         #region Methodes
+
         public List<string> LireFichierMots(string filename)
         {
+
             List<string> dico = new List<string>();
             string[] lignes = File.ReadAllLines(filename);
+
             foreach (string line in lignes)
             {
-                string[] motsParLigne = line.Trim().Split(' ');
+
+                string[] motsParLigne = line.Split(' ');
+
                 foreach (string mot in motsParLigne)
                 {
+
                     dico.Add(mot);
                 }
             }
+
             return dico;
         }
 
@@ -55,9 +67,10 @@ namespace Projet
 
             foreach (string mot in this.mots)
             {
+
                 int longueur = mot.Length;
 
-                if(!motsParLongueur.ContainsKey(longueur)) // si dans le dico il n'y a pas cette longueur la
+                if (!motsParLongueur.ContainsKey(longueur)) // si dans le dico il n'y a pas cette longueur la
                 {
                     motsParLongueur[longueur] = new List<string>(); // alors on créé une nouvelle liste associée à une nv longuer
                 }
@@ -66,7 +79,7 @@ namespace Projet
             }
 
             // tri de chaque liste
-            foreach (int i in  motsParLongueur.Keys)
+            foreach (int i in motsParLongueur.Keys)
             {
                 motsParLongueur[i].Sort();
             }
@@ -77,19 +90,17 @@ namespace Projet
         public Dictionary<char, List<string>> TriMotsParLettre()
         {
             Dictionary<char, List<string>> motsParLettre = new Dictionary<char, List<string>>();
-            
+
             foreach (string mot in this.mots)
             {
                 char c = mot[0]; // premier caractère du mot
 
                 if (!motsParLettre.ContainsKey(c)) // si dans le dico il n'y a pas de mot commencant par cette lettre
-                { 
+                {
                     motsParLettre[c] = new List<string>(); // alors on créé une nouvelle liste associée à une nv longuer
                 }
                 motsParLettre[c].Add(mot); // on ajoute le mots à la liste de mots 
             }
-            
-
 
             // tri de chaque liste
             foreach (char c in motsParLettre.Keys)
@@ -100,63 +111,44 @@ namespace Projet
             return motsParLettre;
         }
 
-        public int MotsLongueur(int longueur)
-        {
-            int occ = 0;
-            foreach (string mot in mots)
-            {
-                if (mot.Length == longueur) { occ++; }
-            }
-            return occ;
-        }
-
-        public int MotsParLettre(char lettre)
-        {
-
-            int occ = 0;
-            foreach (string mot in mots)
-            {
-                string l = lettre.ToString();
-                l = l.ToUpper();
-                lettre = Convert.ToChar(l);
-                if (mot.Length != 0)
-                {
-                    if (mot[0] == lettre) { occ++; }
-                }
-
-            }
-            return occ;
-        }
-
         public string toString()
         {
             string s = "";
+
             for (int i = 2; i <= 24; i++)
             {
-                int occ = MotsLongueur(i);
-                if (occ != 0)
+                if (motsParLongueur.ContainsKey(i))
                 {
+                    int occ = motsParLongueur[i].Count;
                     s = s + i + " : " + occ + "; ";
                 }
             }
+
             s = s + "\n";
             string alphabet = "AZERTYUIOPQSDFGHJKLMWXCVBN";
+
             foreach (char lettre in alphabet)
             {
-                int occ = MotsParLettre(lettre);
-                s = s + lettre + " : " + occ + " ; ";
+                if (motsParLettres.ContainsKey(lettre))
+                {
+                    int occ = motsParLettres[lettre].Count;
+                    s = s + lettre + " : " + occ + " ; ";
+                }
             }
+
             return s;
         }
 
 
         public bool RechercheDichoRecursif(string mot, int deb = 0, int fin = -1)
         {
-            if (!motsParLongueur.ContainsKey(mot.Length)) 
+            if (!motsParLongueur.ContainsKey(mot.Length))
             {
+                Console.WriteLine("c'est containskey qui foire");
+
                 return false;
             }
-                
+
 
             List<string> mots = motsParLongueur[mot.Length];
 
@@ -167,27 +159,21 @@ namespace Projet
 
             if (deb > fin)
             {
+                Console.WriteLine("il n'appartient aps");
+
                 return false;
             }
-                
+
 
             int mil = (deb + fin) / 2;
 
-            if (mot == mots[mil])
-            {
-                return true;
-            }
-            
+            if (mot == mots[mil]) return true;
 
-            if (String.Compare(mot, mots[mil]) < 0)
-            {
-                return RechercheDichoRecursif(mot, deb, mil - 1);
-            }
-            else
-            {
-                return RechercheDichoRecursif(mot, mil + 1, fin);
-            }
+            if (String.Compare(mot, mots[mil]) < 0) return RechercheDichoRecursif(mot, deb, mil - 1);
+
+            else return RechercheDichoRecursif(mot, mil + 1, fin);
         }
+
         public bool Contains(string mot)
         {
             // on suppose que le dictionnaire est dans l'ordre alphabétiqeu donc trié
